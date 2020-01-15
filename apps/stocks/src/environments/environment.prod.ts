cbee8f48a7fcc58b28806a0d918d8e7462275e79
tjhernandez34 <tjhernandez34@gmail.com>
2020-01-15 03:26:15 -0800

    Point UI to proxy server

--- apps/stocks/src/environments/environment.prod.ts
+++ apps/stocks/src/environments/environment.prod.ts
@@ -2,6 +2,5 @@ import { StocksAppConfig } from '@coding-challenge/stocks/data-access-app-config
 
 export const environment: StocksAppConfig = {
   production: true,
-  apiKey: '',
   apiURL: 'https://sandbox.iexapis.com'
 };
