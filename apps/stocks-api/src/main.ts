6eef3114e8d4f1fe7c4f42d1a70e17818110ba3b
tjhernandez34 <tjhernandez34@gmail.com>
2020-01-15 06:02:46 -0800

    Add redis caching layer to api/stock-price endpoint

--- apps/stocks-api/src/main.ts
+++ apps/stocks-api/src/main.ts
@@ -3,10 +3,15 @@
  * This is only a minimal backend to get started.
  **/
 import { Server } from 'hapi';
+import * as Redis from 'redis';
 import * as Wreck from '@hapi/wreck';
 import { environment } from './environments/environment';
+import { promisify } from 'util';
 
 const init = async () => {
+  const redisClient = Redis.createClient(6379);
+  const getAsync = promisify(redisClient.get).bind(redisClient);
+
   const server = new Server({
     port: 3333,
     host: 'localhost',
@@ -22,12 +27,28 @@ const init = async () => {
     path: '/api/stock-price',
     handler: (request, h) => {
       const params = request.query;
-      return Wreck.request(
-        'get',
-        `${environment.apiURL}/beta/stock/${params.symbol}/chart/${
-          params.period
-        }?token=${environment.apiKey}`
-      );
+      const redisSearchKey = `${params.symbol}:${params.period}`;
+      const expirationTime = 60 * 60; // 1 hour cache for Redis;
+
+      console.log(redisSearchKey);
+      return getAsync(redisSearchKey).then(results => {
+        if (results) {
+          console.log('results');
+          return h.response(JSON.parse(results));
+        }
+
+        console.log('no results');
+        return Wreck.request(
+          'get',
+          `${environment.apiURL}/beta/stock/${params.symbol}/chart/${
+            params.period
+          }?token=${environment.apiKey}`
+        ).then(async resp => {
+          const data = await Wreck.read(resp);
+          redisClient.setex(redisSearchKey, expirationTime, data.toString());
+          return h.response(JSON.parse(data.toString()));
+        });
+      });
     }
   });
 
