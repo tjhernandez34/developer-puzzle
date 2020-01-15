ee1a7c07423ad26dda755f8b373fcbedaee92b39
tjhernandez34 <tjhernandez34@gmail.com>
2020-01-15 08:29:30 -0800

    Change ref for prod apiUrl to proxy for consistency

--- apps/stocks/src/environments/environment.prod.ts
+++ apps/stocks/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { StocksAppConfig } from '@coding-challenge/stocks/data-access-app-config
 
 export const environment: StocksAppConfig = {
   production: true,
-  apiURL: 'https://sandbox.iexapis.com'
+  apiURL: 'http://localhost:3333/api/stock-price'
 };
